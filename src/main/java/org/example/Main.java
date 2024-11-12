094d81345c17582aebe5f52ae70225424a4cf481
DavidHuynh04 <131493560+DavidHuynh04@users.noreply.github.com>
2024-11-12 02:04:09 -0800

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    private static void verificationChecker(List<List<String>> userData, int[] userCredibility){
+        for (int i = 0; i < userData.size(); i++){
+            if (Objects.equals(userData.get(i).get(6), "false")){
+                userCredibility[i] -= 2;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        List<List<String>> userData = new ArrayList<>();
+        try (InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("bot_detection_data.csv");
+             CSVReader reader = new CSVReader(new InputStreamReader(inputStream))) {
+            while(reader.readNext()!= null) {
+                userData.add(Arrays.asList(reader.readNext()));
+            }
+            int[] userCredibility = new int[userData.size()];
+            Arrays.fill(userCredibility, 10);
+            verificationChecker(userData, userCredibility);
+        }
+        catch(IOException e){
+                e.printStackTrace();
+            } catch (CsvValidationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
